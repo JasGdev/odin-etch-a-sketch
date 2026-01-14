4453f61f8aa9cd0dcc1c7d4046a32683cdf64c4b
Jason <jason13.deng@gmail.com>
2026-01-14 17:26:31 +0900

    Implement state switching on effects

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
-const body = document.querySelector('body')
-const container = document.querySelector('#container')
+const body = document.querySelector('body');
+const container = document.querySelector('#container');
+
+// 0 is for turn white, 1 is RGB, 2 is darken, 3 is RGB + darken
+var mode = 0;
 
 // create 16x16 grid of square divs
 // put grid squares inside container
@@ -8,12 +11,12 @@ const container = document.querySelector('#container')
 function makeSquareDivs(num){
     squareSize = 500/num;
     for (let i = 0; i < num*num; i++){
-        let square_div = document.createElement('div')
-        square_div.classList.add('squares')
+        let square_div = document.createElement('div');
+        square_div.classList.add('squares');
         square_div.style.cssText = "background: black;";
         square_div.style.width = `${squareSize}px`;
         square_div.style.height = `${squareSize}px`;
-        container.appendChild(square_div)
+        container.appendChild(square_div);
     }
     const squares = document.querySelectorAll(".squares");
 
@@ -46,5 +49,55 @@ bt.addEventListener('click', (e) => {
         bt.dispatchEvent(new Event('click'));
     }
     
+})
+
+// Adding options
+const optionDiv = document.createElement('div');
+optionDiv.classList.add('optional');
+body.insertBefore(optionDiv, container)
+
+const rgbBt = document.createElement('button')
+rgbBt.classList.add('rgb');
+rgbBt.textContent = "RGB effects"
+
+const darkenBt = document.createElement('button')
+darkenBt.classList.add('darken');
+darkenBt.textContent = "Darken Effects"
+
+optionDiv.appendChild(rgbBt)
+optionDiv.appendChild(darkenBt)
+
+// for RGB
+// if clicked toggle button background 
+// if mode = 0 => 1
+// if mode = 1 => 0
+// if mode = 2 => 3
+// if mode = 3 => 2
+rgbBt.addEventListener('click', (e) => {
+    rgbBt.classList.toggle("active");
+    if (mode == 0){mode = 1} 
+    else if (mode == 1) {mode = 0}
+    else if (mode == 2) {mode = 3}
+    else {mode = 2}
+})
+
+
+// for darken
+// if clicked toggle button background 
+// if mode = 0 => 2
+// if mode = 2 => 0
+// if mode = 1 => 3
+// if mode = 3 => 1
+
+darkenBt.addEventListener('click', (e) => {
+    darkenBt.classList.toggle("active");
+})
+
+darkenBt.addEventListener('click', (e) => {
+    darkenBt.classList.toggle("active");
+    if (mode == 0){mode = 2} 
+    else if (mode == 2) {mode = 0}
+    else if (mode == 1) {mode = 3}
+    else {mode = 1}
 })
 
