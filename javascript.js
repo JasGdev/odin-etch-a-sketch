98494fd61517eafed9b281e3fcccc3d241c8bcea
Jason <jason13.deng@gmail.com>
2026-01-14 16:42:00 +0900

    Complete except Extra credit

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body')
 const container = document.querySelector('#container')
 
 // create 16x16 grid of square divs
@@ -14,22 +15,36 @@ function makeSquareDivs(num){
         square_div.style.height = `${squareSize}px`;
         container.appendChild(square_div)
     }
+    const squares = document.querySelectorAll(".squares");
+
+    for (square of squares){
+        square.addEventListener('mouseenter', (e) =>
+            e.target.style.backgroundColor = "white");
+    }
 }
 
 makeSquareDivs(10)
 
-//issue is that all of them are turning hte last square white
-function turnWhite(item){
-    item.style.backgroundColor = "white";
-    console.log("White");
-}
-const squares = document.querySelectorAll(".squares");
-console.log(squares)
-for (square of squares){
-    square.addEventListener('mouseenter', 
-        console.log(square);
-}
-console.log(squares)
 
 
+
+// Adding button
+const bt = document.createElement('button')
+bt.textContent = "New Grid"
+body.insertBefore(bt, container);
+bt.addEventListener('click', (e) => {
+    // Adding prompt
+    let numSides = prompt("Number of squares per side for the new grid?", "2-100");
+    
+    if (numSides <= 100){
+        // existing grid removed
+        container.innerHTML = ""
+        // new grid is generated
+        makeSquareDivs(numSides)
+    } else {
+        alert("Please enter from 2 to 100")
+        bt.dispatchEvent(new Event('click'));
+    }
+    
+})
 
